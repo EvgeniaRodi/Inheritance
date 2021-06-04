74805b4f83c64e166726c0a3acca51b51d93b7f3
Rodionova Evgenia <jenechkajj353@gmail.com>
2021-06-04 10:10:09 +0300

    newtest

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -25,10 +25,10 @@ public class ProductManagerTest {
     @InjectMocks
     ProductManager manager;
 
-    Product first = new Book(1, "novel", 300, "Lermontov");
-    Product second = new Book(2, "poem", 200, "Pushkin");
-    Product third = new Smartphone(3, "Iphone", 55000, "China");
-    Product fourth = new Smartphone(4, "Huawei", 33000, "China");
+    Product first = new Book(1, "Novel", 500, "Agami");
+    Product second = new Book(2, "Memories", 300, "Memoirs");
+    Product third = new Smartphone(3, "Samsung", 57000, "China");
+    Product fourth = new Smartphone(4, "Xiaomi", 15000, "China");
 
     @BeforeEach
     public void SetUp() {
@@ -44,7 +44,7 @@ public class ProductManagerTest {
         Product[] returned = new Product[]{first, second, third, fourth};
         doReturn(returned).when(repository).findAll();
         Product[] expected = new Product[]{second};
-        Product[] actual = manager.searchBy("poem");
+        Product[] actual = manager.searchBy("Memories");
         assertArrayEquals(expected, actual);
 
     }
@@ -87,7 +87,7 @@ public class ProductManagerTest {
         doReturn(returned).when(repository).findAll();
 
         Product[] expected = new Product[]{first};
-        Product[] actual = manager.searchBy("Lermontov");
+        Product[] actual = manager.searchBy("Agami");
         assertArrayEquals(expected, actual);
     }
 
@@ -102,7 +102,7 @@ public class ProductManagerTest {
         doReturn(returned).when(repository).findAll();
 
         Product[] expected = new Product[]{second};
-        Product[] actual = manager.searchBy("Pushkin");
+        Product[] actual = manager.searchBy("Memoirs");
         assertArrayEquals(expected, actual);
     }
 
@@ -111,7 +111,7 @@ public class ProductManagerTest {
         Product[] returned = new Product[]{first, second, third, fourth};
         doReturn(returned).when(repository).findAll();
         Product[] expected = new Product[]{first};
-        Product[] actual = manager.searchBy("Lermontov");
+        Product[] actual = manager.searchBy("Agami");
         assertArrayEquals(expected, actual);
 
     }
@@ -120,7 +120,7 @@ public class ProductManagerTest {
         Product[] returned = new Product[]{first, second, third, fourth};
         doReturn(returned).when(repository).findAll();
         Product[] expected = new Product[]{fourth};
-        Product[] actual = manager.searchBy("Huawei");
+        Product[] actual = manager.searchBy("Xiaomi");
         assertArrayEquals(expected, actual);
 
     }
